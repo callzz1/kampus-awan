e2d3f9be412c58c942df8f2b84daf4bbfaa8e7fa
windupratama <winduprtma@gmail.com>
2024-05-07 00:17:22 +0700

    update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -10,9 +10,9 @@ function Home() {
                 title="Notice: Karena adanya limitasi free tier vercel, fetching data mungkin akan terjadi timeout gateway!"
             />            
             <Banner />
-            <CourseList title="Rekomendasi untuk anda" query={{ sort: "random", limit: 5 }} />
-            <CourseList title="Populer" query={{ order: "name", sort: "random", limit: 5  }} />
-            <CourseList title="Baru ditambahkan" query={{ order:"createdAt", sort: "desc", limit: 5  }} />
+            <CourseList title="Rekomendasi untuk anda" query={{ sort: "random", limit: 6 }} />
+            <CourseList title="Populer" query={{ order: "name", sort: "random", limit: 6  }} />
+            <CourseList title="Baru ditambahkan" query={{ order:"createdAt", sort: "desc", limit: 6  }} />
         </div>
     )
 }
