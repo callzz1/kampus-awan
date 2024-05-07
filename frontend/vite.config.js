bbdcf7602edd6044963ba89769e7c52512155476
windupratama <winduprtma@gmail.com>
2024-05-07 20:17:42 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-undef */
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +14,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: process.env.VITE_PROXY_URL,
       },
     },
   },
